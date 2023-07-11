9e25ff360d01beb347a1351ef914237f10967857
stanciudrg <stanciudragos1337@gmail.com>
2023-07-11 16:40:36 +0300

    Create basic operations functionality
    The operate function adds the following functionalities to the
    calculator: addition, subtraction, multiplication, and
    division

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ let numberButtons = document.querySelectorAll('.number-buttons')
 let operatorButtons = document.querySelectorAll('.operator-buttons')
 let equalsButton = document.querySelector('#equals')
 let clearButton = document.querySelector('#clear')
+
+function operate() {
+
+    switch (calculator.operator) {
+        case ' + ':
+            output.textContent = add()
+            calculator.number1 = [add()];
+            calculator.number2 = [];
+            break;
+        case ' - ':
+            output.textContent = subtract();
+            calculator.number1 = [subtract()];
+            calculator.number2 = [];
+            break;
+        case ' x ':
+            output.textContent = multiply();
+            calculator.number1 = [multiply()];
+            calculator.number2 = [];
+            break;
+        case ' / ':
+            output.textContent = divide();
+            calculator.number1 = [divide()];
+            calculator.number2 = [];
+            break;
+    }
+
+    function add() { return Number(calculator.number1.join('')) + Number(calculator.number2.join('')) };
+    function subtract() { return Number(calculator.number1.join('')) - Number(calculator.number2.join('')) };
+    function multiply() { return Number(calculator.number1.join('')) * Number(calculator.number2.join('')) };
+    function divide() { return Number(calculator.number1.join('')) / Number(calculator.number2.join('')) };
+
+}
