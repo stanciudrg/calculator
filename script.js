9daf936cf88a1feda23c332fd42ef54af0750405
stanciudrg <stanciudragos1337@gmail.com>
2023-07-11 16:38:32 +0300

    Store all buttons in variables and create the calculator object
    The calculator object stores the first operand, the operator,
    the second operand, and the operating state of the calculator

---
+++ script.js
@@ -0,0 +1,12 @@
+let calculator = {
+    number1: [],
+    number2: [],
+    operator: undefined,
+    operating: false,
+}
+
+let output = document.querySelector('.output')
+let numberButtons = document.querySelectorAll('.number-buttons')
+let operatorButtons = document.querySelectorAll('.operator-buttons')
+let equalsButton = document.querySelector('#equals')
+let clearButton = document.querySelector('#clear')
