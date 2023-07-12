dff293fa31429deac65c4f748f7ab8394a4150a9
stanciudrg <stanciudragos1337@gmail.com>
2023-07-12 16:33:18 +0300

    Add function that rounds the operation result to two decimal places

--- script.js
+++ script.js
@@ -32,32 +32,31 @@ function operate() {
 
     switch (calculator.operator) {
         case ' + ':
-            output.textContent = add()
-            calculator.number1 = [add()];
+            calculator.number1 = add().split('');
             calculator.number2 = [];
+            output.textContent = calculator.number1.join('');
             break;
         case ' - ':
-            output.textContent = subtract();
-            calculator.number1 = [subtract()];
+            calculator.number1 = subtract().split('');
             calculator.number2 = [];
+            output.textContent = calculator.number1.join('');;
             break;
         case ' x ':
-            output.textContent = multiply();
-            calculator.number1 = [multiply()];
+            calculator.number1 = multiply().split('');
             calculator.number2 = [];
+            output.textContent = calculator.number1.join('');
             break;
         case ' / ':
-            output.textContent = divide();
-            calculator.number1 = [divide()];
+            calculator.number1 = divide().split('');
             calculator.number2 = [];
+            output.textContent = calculator.number1.join('');
             break;
     }
-
-    function add() { return Number(calculator.number1.join('')) + Number(calculator.number2.join('')) };
-    function subtract() { return Number(calculator.number1.join('')) - Number(calculator.number2.join('')) };
-    function multiply() { return Number(calculator.number1.join('')) * Number(calculator.number2.join('')) };
-    function divide() { return Number(calculator.number1.join('')) / Number(calculator.number2.join('')) };
-
+    function roundDecimals(number) { return number.toFixed(4).replace(/\.?0*$/, ''); }
+    function add() { return roundDecimals(Number(calculator.number1.join('')) + Number(calculator.number2.join(''))) };
+    function subtract() { return roundDecimals(Number(calculator.number1.join('')) - Number(calculator.number2.join(''))) };
+    function multiply() { return roundDecimals(Number(calculator.number1.join('')) * Number(calculator.number2.join(''))) };
+    function divide() { return roundDecimals(Number(calculator.number1.join('')) / Number(calculator.number2.join(''))) };
 }
 
 function selectOperator(e) {
