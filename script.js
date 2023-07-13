c9dc38ed3e00668aac7454b13fa9d22fe5fd5c49
stanciudrg <stanciudragos1337@gmail.com>
2023-07-13 16:45:34 +0300

    Stop operate() from running if number1 or number2 is empty

--- script.js
+++ script.js
@@ -15,38 +15,55 @@ equalsButton.addEventListener('click', equalize);
 clearButton.addEventListener('click', clear);
 
 function operate() {
+
+
+
     switch (operator) {
+
         case ' + ':
+
             number1 = add().split('');
             number2 = [];
             output.textContent = number1.join('');
             break;
+
         case ' - ':
+
             number1 = subtract().split('');
             number2 = [];
             output.textContent = number1.join('');;
             break;
+
         case ' x ':
+
             number1 = multiply().split('');
             number2 = [];
             output.textContent = number1.join('');
             break;
+
         case ' / ':
+
             if (number1 == 0 || number2 == 0) {
+
                 clear()
                 output.textContent = 'ERROR';
+
             } else {
+
                 output.textContent = divide();
                 number1 = divide().split('');
                 number2 = [];
-            }
-            break;
+
+            } break;
+
     }
+
     function roundDecimals(number) { return number.toFixed(4).replace(/\.?0*$/, ''); }
     function add() { return roundDecimals(Number(number1.join('')) + Number(number2.join(''))) };
     function subtract() { return roundDecimals(Number(number1.join('')) - Number(number2.join(''))) };
     function multiply() { return roundDecimals(Number(number1.join('')) * Number(number2.join(''))) };
     function divide() { return roundDecimals(Number(number1.join('')) / Number(number2.join(''))) };
+
 }
 
 function displayNumbers(e) {
